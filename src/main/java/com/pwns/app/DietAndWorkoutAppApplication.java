49d99f34b44be9e18f2f77cfd84a5d7bd7888061
Pedro Willen <pedrowillen28@gmail.com>
2025-03-12 21:50:23 -0400

    feat: add initial project structure

---
+++ src/main/java/com/pwns/app/DietAndWorkoutAppApplication.java
@@ -0,0 +1,13 @@
+package com.pwns.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietAndWorkoutAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DietAndWorkoutAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pwns/app/DietAndWorkoutAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pwns.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietAndWorkoutAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
