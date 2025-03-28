5ca45bf8a3435ad4fd7d479443737f5b4d0795f8
Pedro Willen <pedrowillen28@gmail.com>
2025-03-28 00:11:56 -0400

    feat: add ExerciseSet and WorkoutExercise repositories

---
+++ src/main/java/com/pwns/app/repository/ExerciseSetRepository.java
@@ -0,0 +1,7 @@
+package com.pwns.app.repository;
+
+import com.pwns.app.entity.ExerciseSet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExerciseSetRepository extends JpaRepository<ExerciseSet, Long> {
+}
---
+++ src/main/java/com/pwns/app/repository/WorkoutExerciseRepository.java
@@ -0,0 +1,7 @@
+package com.pwns.app.repository;
+
+import com.pwns.app.entity.WorkoutExercise;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WorkoutExerciseRepository extends JpaRepository<WorkoutExercise, Long> {
+}
