24a1a85a29d34252c1890588998c49306e0a8f1c
Pedro Willen <pedrowillen28@gmail.com>
2025-03-28 00:16:47 -0400

    feat: add more fields to mapper build of UserWorkoutPlanMapper

--- src/main/java/com/pwns/app/mapper/UserWorkoutPlanMapper.java
+++ src/main/java/com/pwns/app/mapper/UserWorkoutPlanMapper.java
@@ -10,21 +10,30 @@ import com.pwns.app.utils.Sex;
 import com.pwns.app.utils.TrainingType;
 import lombok.experimental.UtilityClass;
 
+import java.time.LocalDate;
+
 @UtilityClass
 public class UserWorkoutPlanMapper {
     public static UserWorkoutPlan toUserWorkoutPlan(UserWorkoutPlanRequest request, User user) {
+        LocalDate startDate = request.periodStart() != null ? request.periodStart() : LocalDate.now();
+
         return UserWorkoutPlan.builder()
                 .user(user)
                 .periodizationWeeks(request.periodizationWeeks())
                 .trainingType(TrainingType.valueOf(request.trainingType()))
+                .periodStart(request.periodStart())
+                .periodEnd(startDate.plusWeeks(request.periodizationWeeks()))
                 .build();
     }
 
     public static UserWorkoutPlanResponse toUserWorkoutPlanResponse(UserWorkoutPlan userWorkoutPlan) {
         return UserWorkoutPlanResponse.builder()
+                .id(userWorkoutPlan.getId())
                 .userResponse(UserMapper.toUserResponse(userWorkoutPlan.getUser()))
                 .periodizationWeeks(userWorkoutPlan.getPeriodizationWeeks())
                 .trainingType(userWorkoutPlan.getTrainingType())
+                .periodStart(userWorkoutPlan.getPeriodStart())
+                .periodEnd(userWorkoutPlan.getPeriodEnd())
                 .build();
     }
 }
