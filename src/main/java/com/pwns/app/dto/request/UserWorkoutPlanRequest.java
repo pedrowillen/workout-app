beb06d55b39b89f5c0127f8637276b0baa3825ef
Pedro Willen <pedrowillen28@gmail.com>
2025-03-28 00:14:07 -0400

    feat: add request and response validations in UserWorkoutPlan

--- src/main/java/com/pwns/app/dto/request/UserWorkoutPlanRequest.java
+++ src/main/java/com/pwns/app/dto/request/UserWorkoutPlanRequest.java
@@ -1,11 +1,17 @@
 package com.pwns.app.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.pwns.app.utils.TrainingType;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
+
+import java.time.LocalDate;
 
 @Builder
 public record UserWorkoutPlanRequest(
         Long userId,
-        int periodizationWeeks,
-        String trainingType
+        @NotNull int periodizationWeeks,
+        String trainingType,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy") LocalDate periodStart
 ) { }
--- src/main/java/com/pwns/app/dto/response/UserWorkoutPlanResponse.java
+++ src/main/java/com/pwns/app/dto/response/UserWorkoutPlanResponse.java
@@ -3,9 +3,14 @@ package com.pwns.app.dto.response;
 import com.pwns.app.utils.TrainingType;
 import lombok.Builder;
 
+import java.time.LocalDate;
+
 @Builder
 public record UserWorkoutPlanResponse(
+        Long id,
         UserResponse userResponse,
         int periodizationWeeks,
-        TrainingType trainingType
+        TrainingType trainingType,
+        LocalDate periodStart,
+        LocalDate periodEnd
 ) { }
